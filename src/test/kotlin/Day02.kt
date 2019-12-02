cd5b5cd7e1dcd78c292b8719f6fa92345fd241a4
Christof Vollrath <christof-vollrath@spring-media.de>
2019-12-02 16:48:53 +0100

    Day02 - part 2: work in progressm (IndexOutOfBoundsException)

--- src/test/kotlin/Day02.kt
+++ src/test/kotlin/Day02.kt
@@ -84,9 +84,70 @@ to the "1202 program alarm" state it had just before the last computer caught fi
 To do this, before running the program, replace position 1 with the value 12 and replace position 2 with the value 2.
 What value is left at position 0 after the program halts?
 
+--- Part Two ---
+
+"Good, the new computer seems to be working correctly!
+Keep it nearby during this mission - you'll probably use it again.
+Real Intcode computers support many more features than your new one,
+but we'll let you know what they are as you need them."
+
+"However, your current priority should be to complete your gravity assist around the Moon.
+For this mission to succeed, we should settle on some terminology for the parts you've already built."
+
+Intcode programs are given as a list of integers; these values are used as the initial state for the computer's memory.
+When you run an Intcode program, make sure to start by initializing memory to the program's values.
+A position in memory is called an address (for example, the first value in memory is at "address 0").
+
+Opcodes (like 1, 2, or 99) mark the beginning of an instruction.
+The values used immediately after an opcode, if any, are called the instruction's parameters.
+For example, in the instruction 1,2,3,4, 1 is the opcode; 2, 3, and 4 are the parameters.
+The instruction 99 contains only an opcode and has no parameters.
+
+The address of the current instruction is called the instruction pointer; it starts at 0.
+After an instruction finishes, the instruction pointer increases by the number of values in the instruction;
+until you add more instructions to the computer, this is always 4 (1 opcode + 3 parameters)
+for the add and multiply instructions.
+(The halt instruction would increase the instruction pointer by 1, but it halts the program instead.)
+
+"With terminology out of the way, we're ready to proceed.
+To complete the gravity assist, you need to determine what pair of inputs produces the output 19690720."
+
+The inputs should still be provided to the program by replacing the values at addresses 1 and 2, just like before.
+In this program, the value placed in address 1 is called the noun, and the value placed in address 2 is called the verb.
+Each of the two input values will be between 0 and 99, inclusive.
+
+Once the program has halted, its output is available at address 0, also just like before.
+Each time you try a pair of inputs, make sure you first reset the computer's memory to the values in the program
+(your puzzle input) - in other words, don't reuse memory from a previous attempt.
+
+Find the input noun and verb that cause the program to produce the output 19690720.
+What is 100 * noun + verb? (For example, if noun=12 and verb=2, the answer would be 1202.)
+
 */
 
 val exerciseInputString = "1,0,0,3,1,1,2,3,1,3,4,3,1,5,0,3,2,1,9,19,1,19,5,23,2,23,13,27,1,10,27,31,2,31,6,35,1,5,35,39,1,39,10,43,2,9,43,47,1,47,5,51,2,51,9,55,1,13,55,59,1,13,59,63,1,6,63,67,2,13,67,71,1,10,71,75,2,13,75,79,1,5,79,83,2,83,9,87,2,87,13,91,1,91,5,95,2,9,95,99,1,99,5,103,1,2,103,107,1,10,107,0,99,2,14,0,0"
+
+fun List<Int>.executeIntCodes(): List<Int> {
+    val currentState = toMutableList()
+    var currentIndex = 0
+    while(true) {
+        when(currentState.getOrNull(currentIndex)) {
+            1 -> {
+                if (currentIndex + 3 >= currentState.size - 1) return currentState.toList()
+                currentState[currentState[currentIndex + 3]] = currentState[currentState[currentIndex + 1]] + currentState[currentState[currentIndex + 2]]
+                currentIndex += 4
+            }
+            2 -> {
+                if (currentIndex + 3 >= currentState.size - 1) return currentState.toList()
+                currentState[currentState[currentIndex + 3]] = currentState[currentState[currentIndex + 1]] * currentState[currentState[currentIndex + 2]]
+                currentIndex += 4
+            }
+            99, null -> return currentState.toList()
+        }
+    }
+}
+
+fun parseIntCodes(inputString: String): List<Int> = inputString.split(",").map { it.trim().toInt() }
 
 class Day02Spec : Spek({
 
@@ -118,27 +179,40 @@ class Day02Spec : Spek({
             }
         }
     }
+    describe("part 2") {
+        given("example input from part 1") {
+            val exerciseInput = parseIntCodes(exerciseInputString)
+            on("find none and verb") {
+                val (none, verb) = findNoneAndVerb(exerciseInput, 3895705)!!
+                it("should have the right result") {
+                    none `should equal` 12
+                    verb `should equal` 2
+                }
+            }
+        }
+        given("exercise input") {
+            val exerciseInput = parseIntCodes(exerciseInputString).toMutableList()
+            on("find none and verb") {
+                val (none, verb) = findNoneAndVerb(exerciseInput, 19690720)!!
+                it("should have the right result") {
+                    none `should equal` 12
+                    verb `should equal` 2
+                }
+            }
+        }
+
+    }
+
 })
 
-private fun List<Int>.executeIntCodes(): List<Int> {
-    val currentState = toMutableList()
-    var currentIndex = 0
-    while(true) {
-        println(currentIndex)
-        println(currentState)
-        when(currentState[currentIndex]) {
-            1 -> {
-                currentState[currentState[currentIndex + 3]] = currentState[currentState[currentIndex + 1]] + currentState[currentState[currentIndex + 2]]
-                currentIndex += 4
-            }
-            2 -> {
-                println("${currentIndex + 3}")
-                currentState[currentState[currentIndex + 3]] = currentState[currentState[currentIndex + 1]] * currentState[currentState[currentIndex + 2]]
-                currentIndex += 4
-            }
-            99 -> return currentState.toList()
+fun findNoneAndVerb(input: List<Int>, expected: Int): Pair<Int, Int>? {
+    for (none in 0..1000)
+        for (verb in 0..1000) {
+            val probeInput = input.toMutableList()
+            probeInput[1] = none
+            probeInput[2] = verb
+            val result = probeInput.executeIntCodes()
+            if (result[0] == expected) return none to verb
         }
-    }
+    throw IllegalArgumentException("No none/verb found for input in time box")
 }
-
-fun parseIntCodes(inputString: String): List<Int> = inputString.split(",").map { it.trim().toInt() }
