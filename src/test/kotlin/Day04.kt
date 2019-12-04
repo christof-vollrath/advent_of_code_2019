843e076f7b3ebfe57047237978c4e5695ba081bc
Christof <christof@taobits.net>
2019-12-04 13:28:14 +0100

    Day04 - part 1 completed

--- src/test/kotlin/Day04.kt
+++ src/test/kotlin/Day04.kt
@@ -1,6 +1,15 @@
+import org.amshove.kluent.`should equal`
+import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.describe
+import org.jetbrains.spek.api.dsl.it
+import org.jetbrains.spek.data_driven.data
+import org.jetbrains.spek.data_driven.on as onData
+
 /*
 --- Day 4: Secure Container ---
-You arrive at the Venus fuel depot only to discover it's protected by a password. The Elves had written the password on a sticky note, but someone threw it out.
+
+You arrive at the Venus fuel depot only to discover it's protected by a password.
+The Elves had written the password on a sticky note, but someone threw it out.
 
 However, they do remember a few key facts about the password:
 
@@ -16,4 +25,62 @@ Other than the range rule, the following are true:
 How many different passwords within the range given in your puzzle input meet these criteria?
 
 Your puzzle input is 264793-803935.
+
  */
+
+class Day04Spec : Spek({
+
+    describe("part 1") {
+        describe("enumerate all numbers in range") {
+            val count = candidates().count()
+            count `should equal` 803935 - 264793 + 1
+        }
+        describe("calculate required fuel") {
+            val testData = arrayOf(
+                data(111111, true),
+                data(223450, false),
+                data(123789, false)
+            )
+            onData("fallows password rules %s", with = *testData) { password, expected ->
+                it("should return $expected") {
+                    fallowsPasswordRules(password) `should equal` expected
+                }
+            }
+        }
+        describe("count passwords") {
+            val count = candidates().filter {fallowsPasswordRules(it) }.count()
+            count `should equal` 966
+        }
+    }
+})
+
+fun fallowsPasswordRules(password: Int): Boolean {
+    val chars = password.toString().toList()
+    return sixDigits(chars) && containsTwins(chars) && neverDecreasingDigits(chars)
+}
+
+fun neverDecreasingDigits(chars: List<Char>): Boolean {
+    chars.drop(1).fold(chars.first()) { prev, current ->
+        if (prev > current) return false
+        current
+    }
+    return true
+}
+
+fun containsTwins(chars: List<Char>): Boolean {
+    chars.drop(1).fold(chars.first()) { prev, current ->
+        if (prev == current) return true
+        current
+    }
+    return false
+}
+
+fun sixDigits(chars: List<Char>) = chars.size == 6
+
+fun candidates(): Sequence<Int> {
+    return sequence {
+        for (i in 264793..803935) {
+            yield(i)
+        }
+    }
+}
