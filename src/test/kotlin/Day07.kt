ab15d5eac15e22aea45d4c2b5e2fe3632c427099
Christof Vollrath <christof@taobits.net>
2019-12-12 21:59:51 +0100

    Day07 - part2 completed

--- src/test/kotlin/Day07.kt
+++ src/test/kotlin/Day07.kt
@@ -232,9 +232,33 @@ class Day07Spec : Spek({
                 }
             }
         }
+        describe("find best phase settings looped (example 1)") {
+            val intCodesString = "3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5"
+            val intCodes = parseIntCodes(intCodesString)
+            it("should find best settings") {
+                val result = findBestPhaseSettingsLooped(intCodes)
+                result `should equal` (listOf(9,8,7,6,5) to 139629729)
+            }
+        }
+        describe("find best phase settings looped (example 2)") {
+            val intCodesString = "3,52,1001,52,-5,52,3,53,1,52,56,54,1007,54,5,55,1005,55,26,1001,54,-5,54,1105,1,12,1,53,54,53,1008,54,0,55,1001,55,1,55,2,53,55,53,4,53,1001,56,-1,56,1005,56,6,99,0,0,0,0,10"
+            val intCodes = parseIntCodes(intCodesString)
+            it("should find best settings") {
+                val result = findBestPhaseSettingsLooped(intCodes)
+                result `should equal` (listOf(9,7,8,5,6) to 18216)
+            }
+        }
+        describe("exercise") {
+            val intCodesString = readResource("day07Input.txt")!!
+            val intCodes = parseIntCodes(intCodesString)
+            it("should find best settings") {
+                val result = findBestPhaseSettingsLooped(intCodes)
+                result `should equal` (listOf(8, 6, 7, 9, 5) to 9246095)
+            }
+        }
+
     }
 })
-
 
 fun findBestPhaseSettings(intCodes: List<Int>): Pair<List<Int>, Int> =
     listOf(0, 1, 2, 3, 4).permute().map { phaseSettings ->
@@ -281,3 +305,8 @@ fun runLoopedIntCodes(intCodes: List<Int>, phaseSettings: List<Int>, looped: Boo
         } else outputChannel.receive()
     }
 }
+fun findBestPhaseSettingsLooped(intCodes: List<Int>): Pair<List<Int>, Int> =
+    listOf(5, 6, 7, 8, 9).permute().map { phaseSettings ->
+        val output = runLoopedIntCodes(intCodes, phaseSettings, looped = true)
+        phaseSettings to output
+    }.maxBy { it.second }!!
